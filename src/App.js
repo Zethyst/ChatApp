874e1affe3814f9a01094e66c31bbb58e789304f
Ekansh <zethyst@protonmail.com>
2023-10-11 01:29:44 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {ChatEngine} from 'react-chat-engine'; 
+import React, { Component, useEffect } from 'react';
+import ChatFeed from './components/ChatFeed.jsx';
+import LoginForm from './components/LoginForm';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { getElementError } from '@testing-library/react';
+
+//npm i @ant-design/icons
+//npm i axios
+//npm i react-chat-engine
+//ChatEngine with its various props
 
 function App() {
+  
+  useEffect(() => {
+
+    const clearInputByClassName = () => {
+      setTimeout(()=>{
+        const inputElement = document.querySelector('.ce-text-input');
+        // alert(inputElement)
+        if (inputElement) {
+          inputElement.value = '';
+        }
+      },10)
+    };
+
+    clearInputByClassName();
+  }, []); // The empty dependency array ensures that the effect runs only once after the initial render
+
+  if (!localStorage.getItem("username")) {
+    return <LoginForm/> 
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine
+        height="100vh"
+        projectID="83b0d608-5157-4c0e-a7cb-37d16f6a3f8d"
+        // userName="zethyst"
+        // userSecret="qwerty"
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed = { (chatAppProps)=> <ChatFeed {...chatAppProps}/>}
+        onNewMessage={()=> new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3')}
+      />
+
     </div>
   );
 }
